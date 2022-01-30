660ac3560b5d263e0ff53ed657b148a9d887fc1d
Galdino <mateusgaldino1000@hotmail.com>
2022-01-30 02:23:19 -0300

    feat: finish choices modal implementation

--- src/components/BotModal/styles.ts
+++ src/components/BotModal/styles.ts
@@ -11,7 +11,7 @@ export const ModalContainer = styled.div<IModalContainerProps>`
   width: 100%;
   height: 100%;
   align-items: flex-end;
-  z-index: 2;
+  z-index: 1000;
   display: none;
 
   ${({ isModalOpen }) =>
